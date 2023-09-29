53951e75aa4fcd0646f30b4712b4c7f3b0c261fc
prateek <prateek.a.verma@capgemini.com>
2023-09-29 21:13:38 +0530

    CUDA version check added

--- userdata/post_reboot.sh
+++ userdata/post_reboot.sh
@@ -13,14 +13,15 @@ fi
 # Extract specific values from the YAML file
 PYTORCH_VERSION=$(yq eval '.Pytorch' ./config.yaml)
 PYTHON_VERSION=$(yq eval '.Python' ./config.yaml)
+CUDA_VERSION=$(yq eval '.CUDA' ./config.yaml)
 
 # Print the extracted values
 echo "PyTorch Version: $PYTORCH_VERSION"
 echo "Python Version: $PYTHON_VERSION"
 
 # Check if PyTorch version is provided
-if [ -z "$PYTORCH_VERSION" ]; then
-    echo "PyTorch version not provided. Skipping installation."
+if [ -z "$CUDA_VERSION" ]; then
+    echo "CUDA version not provided. Skipping installation."
 else
     # Check NVIDIA GPU information after reboot
     echo "Checking NVIDIA GPU information after reboot..."
@@ -36,9 +37,16 @@ else
     .................................................."
 
     # Install PyTorch version
-    echo "Installing PyTorch version $PYTORCH_VERSION"
+    #echo "Installing PyTorch version $PYTORCH_VERSION"
+    echo "Installing PyTorch for CUDA version $CUDA_VERSION"
+    if [$CUDA_VERSION eq 11.7]; then
+        pip install torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
+    if [$CUDA_VERSION eq 11.8]; then
+        pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
+    else
+        echo "Specified CUDA Version not supported..."
+    fi
     #pip3 install "torch $PYTORCH_VERSION"                   #pip3 added #prateek
-    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu     #prateek
 
     # Display the torch installed version
     echo "Installed torch version"

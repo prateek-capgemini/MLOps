36160ad0ce74e141879576a917227a6773caf56f
prateek <prateek.a.verma@capgemini.com>
2023-09-29 21:28:15 +0530

    CUDA version check added #3

--- userdata/post_reboot.sh
+++ userdata/post_reboot.sh
@@ -39,9 +39,9 @@ else
     # Install PyTorch version
     #echo "Installing PyTorch version $PYTORCH_VERSION"
     echo "Installing PyTorch for CUDA version $CUDA_VERSION"
-    if [$CUDA_VERSION eq 11.7]; then
+    if [$CUDA_VERSION -eq 11.7]; then
         pip install torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
-    if [$CUDA_VERSION eq 11.8]; then
+    if [$CUDA_VERSION -eq 11.8]; then
         pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
     else
         echo "Specified CUDA Version not supported..."

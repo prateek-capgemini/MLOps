7f56a30ba2286dd0862419147964bb4f3ee8437b
prateek <prateek.a.verma@capgemini.com>
2023-10-03 16:21:15 +0530

    6th commit

--- userdata/pre_reboot.sh
+++ userdata/pre_reboot.sh
@@ -10,33 +10,15 @@ sudo apt-get install -y alsa-utils
 sudo apt install -y python3-pip
 sudo apt-get install -y ubuntu-drivers-common
 
-#########################################################
-
-# install other import packages
-sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
-
-# first get the PPA repository driver
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt update
-
-# install nvidia driver with dependencies
-sudo apt install libnvidia-common-470
-sudo apt install libnvidia-gl-470
-sudo apt install nvidia-driver-470
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update
-
-#########################################################
-
 # List available Ubuntu drivers and install the recommended driver
 echo "Listing available Ubuntu drivers and installing the recommended driver..."
 sudo ubuntu-drivers list
 sudo ubuntu-drivers autoinstall
 
+
+# Install PyTorch version
+echo "Installing PyTorch version $PYTORCH_VERSION"
+pip install "torch==$PYTORCH_VERSION"
 
 # Reboot the system
 #echo "Rebooting the system..."
@@ -55,26 +37,10 @@ fi
 # Extract specific values from the YAML file
 PYTORCH_VERSION=$(yq eval '.Pytorch' ./config.yaml)
 PYTHON_VERSION=$(yq eval '.Python' ./config.yaml)
-CUDA_VERSION=$(yq eval '.CUDA' ./config.yaml)
 
 # Print the extracted values
 echo "PyTorch Version: $PYTORCH_VERSION"
 echo "Python Version: $PYTHON_VERSION"
-echo "CUDA Version: $CUDA_VERSION"
-
-
-# Install PyTorch version
-echo "Installing PyTorch version $PYTORCH_VERSION"
-pip install "torch==$PYTORCH_VERSION"
-    
-# Display the torch installed version
-echo "Installed torch version"
-pip show torch | grep Version
-python --version
-python3 --version
-
-
-
 
 # Check if PyTorch version is provided
 if [ -z "$PYTORCH_VERSION" ]; then
@@ -93,10 +59,10 @@ else
     .....................................................
     .................................................."
 
-   # Install PyTorch version
+    # Install PyTorch version
     echo "Installing PyTorch version $PYTORCH_VERSION"
     pip install "torch==$PYTORCH_VERSION"
-    
+
     # Display the torch installed version
     echo "Installed torch version"
     pip show torch | grep Version

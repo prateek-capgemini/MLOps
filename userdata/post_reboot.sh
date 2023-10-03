afdbe08a3bea09182d3147990ccdc3b66da6ac71
prateek <prateek.a.verma@capgemini.com>
2023-10-03 12:12:10 +0530

    2nd commit

--- userdata/post_reboot.sh
+++ userdata/post_reboot.sh
@@ -10,10 +10,53 @@ fi
 # Extract specific values from the YAML file
 PYTORCH_VERSION=$(yq eval '.Pytorch' ./config.yaml)
 PYTHON_VERSION=$(yq eval '.Python' ./config.yaml)
+CUDA_VERSION=$(yq eval '.CUDA' ./config.yaml)
 
 # Print the extracted values
 echo "PyTorch Version: $PYTORCH_VERSION"
 echo "Python Version: $PYTHON_VERSION"
+echo "CUDA Version: $CUDA_VERSION"
+
+###########################################################
+
+ # installing CUDA-11.3
+
+if [$CUDA_VERSION=="11.3"]; then
+    echo "Installing CUDA: $CUDA_VERSION ....."
+    sudo apt install cuda-11-3
+else
+    echo "Provided CUDA version not supported "
+fi
+
+# setup your paths
+echo 'export PATH=/usr/local/cuda-11.3/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+source ~/.bashrc
+sudo ldconfig
+if [ -z "$CUDA_VERSION" ]; then
+    echo "CUDA version not provided. Skipping installation."
+else
+    # install cuDNN v11.3
+    
+    CUDNN_TAR_FILE="cudnn-11.3-linux-x64-v8.2.1.32.tgz"
+    wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.1.32/11.3_06072021/cudnn-11.3-linux-x64-v8.2.1.32.tgz
+    tar -xzvf ${CUDNN_TAR_FILE}
+
+    # copy the following files into the cuda toolkit directory.
+    sudo cp -P cuda/include/cudnn.h /usr/local/cuda-11.3/include
+    sudo cp -P cuda/lib64/libcudnn* /usr/local/cuda-11.3/lib64/
+    sudo chmod a+r /usr/local/cuda-11.3/lib64/libcudnn*
+fi
+
+# Finally, to verify the installation, check
+nvidia-smi
+nvcc -V
+
+# install Pytorch
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+
+##########################################################
+
 
 # Check if PyTorch version is provided
 if [ -z "$PYTORCH_VERSION" ]; then
@@ -34,8 +77,12 @@ else
 
     # Install PyTorch version
     echo "Installing PyTorch version $PYTORCH_VERSION"
-    pip install "torch==$PYTORCH_VERSION"
-
+    if [ $PYTORCH_VERSION=="1.12" ]; then
+        pip install "torch==$PYTORCH_VERSION+cu113" torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+    else
+        echo " Provided PYTorch version not supported..."
+    fi
+    
     # Display the torch installed version
     echo "Installed torch version"
     pip show torch | grep Version
--- userdata/pre_reboot.sh
+++ userdata/pre_reboot.sh
@@ -10,6 +10,28 @@ sudo apt-get install -y alsa-utils
 sudo apt install -y python3-pip
 sudo apt-get install -y ubuntu-drivers-common
 
+#########################################################
+
+# install other import packages
+sudo apt-get install g++ freeglut3-dev build-essential libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev
+
+# first get the PPA repository driver
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+
+# install nvidia driver with dependencies
+sudo apt install libnvidia-common-470
+sudo apt install libnvidia-gl-470
+sudo apt install nvidia-driver-470
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt-get update
+
+#########################################################
+
 # List available Ubuntu drivers and install the recommended driver
 echo "Listing available Ubuntu drivers and installing the recommended driver..."
 sudo ubuntu-drivers list

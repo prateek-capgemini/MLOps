91d2461089f495fd2e417b4595d56bfdc82bfdfe
prateek <prateek.a.verma@capgemini.com>
2023-10-05 18:06:53 +0530

    3rd

--- ec2instance.tf
+++ ec2instance.tf
@@ -7,18 +7,13 @@ resource "aws_instance" "ec2" {
   instance_type = local.yaml_rg.Instance_Type
   key_name      = local.yaml_rg.KEY_NAME
 
-  root_block_device {
-    volume_size = 30  # Increase the volume size to 50 GB (default is 8 GB)
-    volume_type = "gp2"
-  }
-
 
   user_data     = <<-EOF
     #!/bin/bash
     
-    sudo su
     # Update package list and install necessary packages
     echo "Updating package list and installing necessary packages..."
+    sudo su
     sudo apt-get update -y
     sudo apt install -y python3-pip 
     sudo apt install -y nvidia-cuda-toolkit
@@ -26,75 +21,36 @@ resource "aws_instance" "ec2" {
     sudo apt-get install -y alsa-utils
     sudo ubuntu-drivers list  
     sudo ubuntu-drivers autoinstall
-    ##################################################################################
-    echo "Updating package list and installing necessary packages..."
-    sudo apt-get update -y
 
-    ############################################################
-    echo "Installing Python virtual environment..."
-    sudo apt install python3.8-venv -y
-
-    # Install yq if not already installed
-    if ! command -v yq &> /dev/null; then
-      echo "yq is not installed. Installing..."
-      sudo apt update
-      sudo snap install yq
-    fi
-
-
-    # Extract specific values from the YAML file
-    PYTORCH_VERSION="1.13"   # $(yq eval '.Pytorch' ./config.yaml)
-    # PYTHON_VERSION=$(yq eval '.Python' ./config.yaml)
-    CUDA_VERSION="11.6"   #$(yq eval '.CUDA' ./config.yaml)
-    PYTHON_VERSION="3.7"
-
-    # Print the extracted values
-    echo "PyTorch Version: $PYTORCH_VERSION"
-    echo "Python Version: $PYTHON_VERSION"
-    echo "CUDA Version: $CUDA_VERSION"
-
-    # Check if PyTorch version is provided
-    if [ -z "$PYTORCH_VERSION" ]; then
-      echo "PyTorch version not provided. Skipping installation."
-    else
-      # Check NVIDIA GPU information after reboot
-      echo "Checking NVIDIA GPU information after reboot..."
-      nvidia-smi
-      python3 -m venv auto_env
-      echo "Increasing Swap Space...."
-      sudo fallocate -l 2G /swapfile
-      sudo chmod 600 /swapfile
-      sudo mkswap /swapfile
-      sudo swapon /swapfile
-
-
-      echo "Building pyton virtual environment...."
-
-      #python3 -m venv auto_env
-      source auto_env/bin/activate
-      sudo pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
-      deactivate
-
-      # Install PyTorch version
-      #echo "Installing PyTorch version $PYTORCH_VERSION"
-      #pip install "torch==$PYTORCH_VERSION"
     
-      # Display the torch installed version
-      echo "Installed torch version"
-      pip show torch | grep Version
-      python --version
-      python3 --version
+    # List available Ubuntu drivers and install the recommended driver
+    echo "Listing available Ubuntu drivers and installing the recommended driver..."
+    sudo modprobe nvidia
 
-      # Run the test script
-      echo "Running the torch validation script..."
-      python3 ./userdata/torch_validation.py
-    fi
+
+    # Check NVIDIA GPU information after reboot
+    echo "Checking NVIDIA GPU information after reboot..."
+    nvidia-smi
+    # Install Python virtual environment
+    echo "Installing Python virtual environment..."
+    sudo apt install -y python3-venv
+    python3 -m venv auto_env
+    source auto_env/bin/activate
+    echo "end of the pyton virtual environment..........................
+    .....................................................
+    .................................................."
+
+    # Install PyTorch version
+    echo "Installing PyTorch version 2.0"
+    pip install torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
+
+    
  
     EOF
 
-  tags = {
-    Name = local.yaml_rg.Instance_Name
-  }
+  # tags = {
+  #   Name = local.yaml_rg.Instance_Name
+  # }
   
   # provisioner "local-exec" {
   #   command = "chmod +x ./userdata/post_reboot.sh"
@@ -109,7 +65,6 @@ resource "aws_instance" "ec2" {
     command = ".\\userdata\\post_reboot.sh"
   }*/
 }
-
 # resource "null_resource" "terminate_instances" {
 #    triggers = {
 #      instance_ids = join(",", concat(aws_instance.cpu_instance.*.id, aws_instance.gpu_instance.*.id))

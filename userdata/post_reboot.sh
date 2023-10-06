60dd2cdaf75436dee9cc1d846a6b525da61ee6b9
prateek <prateek.a.verma@capgemini.com>
2023-10-06 07:30:19 +0530

    legacy code change #6

--- userdata/post_reboot.sh
+++ userdata/post_reboot.sh
@@ -27,20 +27,51 @@ else
 
     # Install Python virtual environment
     echo "Installing Python virtual environment..."
-    sudo apt install -y python3-venv
+    sudo apt install python3.10-venv -y
     sudo su
-    cd /
     sudo python3 -m venv /auto_env
     source /auto_env/bin/activate
+
+    echo "Increasing Swap Space...."
+    sudo fallocate -l 2G /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+
     echo "end of the pyton virtual environment..........................
     .....................................................
     .................................................."
 
-    # Install PyTorch version
-    chmod +x ./userdata/cuda_version_validation.sh
-    ./userdata/cuda_version_validation.sh
+    ################################################################
 
-    #pip install "torch==$PYTORCH_VERSION"
+    # Check if the desired_version matches one of the specified versions
+    if [ "$CUDA_VERSION" == "11.7" ] || [ "$CUDA_VERSION" == "11.6" ] || [ "$CUDA_VERSION" == "11.3" ] || [ "$CUDA_VERSION" == "10.2" ] || [ "$CUDA_VERSION" == "10.1" ] || [ "$CUDA_VERSION" == "10.0" ]; then
+        echo "Installing CUDA version $CUDA_VERSION"
+    
+        # Depending on the version, install PyTorch using pip
+        if [ "$CUDA_VERSION" == "11.7" ]; then
+            pip install torch==2.0.0+cu117 torchvision==0.15.1+cu117 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu117
+        elif [ "$CUDA_VERSION" == "11.6" ]; then
+            pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+        elif [ "$CUDA_VERSION" == "11.3" ]; then
+            pip install torch==1.12.0+cu113 torchvision==0.13.0+cu113 torchaudio==0.12.0 --extra-index-url https://download.pytorch.org/whl/cu113
+        elif [ "$CUDA_VERSION" == "10.2" ]; then
+            pip install torch==1.11.0+cu102 torchvision==0.12.0+cu102 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu102
+        elif [ "$CUDA_VERSION" == "10.1" ]; then
+            echo "PyTorch version 1.10 not support CUDA version 10.1, hence installing CUDA version 10.2 for stable env."
+            pip install torch==1.10.0+cu102 torchvision==0.11.0+cu102 torchaudio==0.10.0 -f https://download.pytorch.org/whl/torch_stable.html
+        elif [ "$CUDA_VERSION" == "10.0" ]; then
+            echo "PyTorch version 1.9 not support CUDA version 10.0, hence installing CUDA version 10.2 for stable env."
+            pip install torch==1.9.0+cu102 torchvision==0.10.0+cu102 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+        fi
+    
+        echo "PyTorch version $PYTORCH_VERSION is now installed."
+    else
+        echo "Unsupported PyTorch version: $PYTORCH_VERSION"
+    fi    
+
+    ################################################################
+
 
     # Display the torch installed version
     echo "Installed torch version"
